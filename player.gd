6b5b5fb6d5b694f643bae431a0882511e1df4032
KeaganA516 <keagan.alcorn@searcyschools.org>
2024-11-14 11:12:40 -0600

    adf

---
+++ player.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	
+	apply_central_force(input * 1200.0 * delta)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+  
